c35838bcf017313a40ed6c074f29adcf21ce8118
Azakiah Vallente <azakiah.vallente@nustarenergy.com>
2022-07-28 14:41:05 -0500

    Added some documentation and removed plaintext credentails

--- ipsla_oid_generator.py
+++ ipsla_oid_generator.py
@@ -1,4 +1,5 @@
 import os
+import json
 import orionsdk
 from jinja2 import FileSystemLoader, PackageLoader, Template, Environment, select_autoescape
 
@@ -7,7 +8,7 @@ def generate_DID(query_list):
     for i in range(len(query_list)):
         duplicate_count = 0
         x = 0
-        while query_list[i-x]['SID'] == query_list[i-x-1]['SID']:
+        while query_list[i-x]['SID'] == query_list[i-x-1]['SID']: # Lookback for duplicates
             x += 1
             duplicate_count += 1
         query_list[i]['DID'] = query_list[i]['SID'] + str(duplicate_count)
@@ -21,8 +22,13 @@ def generate_OID(query_list, operation_type):
 
 
 def main():
+    # Grab creds
+    credential = json.loads((open('private/mymomsenchiladarecipe.json', 'r')).read())
+
     # Initialize connection to orion server
-    swis = orionsdk.SwisClient("txlphqsorion1.usdom1.ad", "admin", "C1sco123!")
+    swis = orionsdk.SwisClient(credential['orionDB']['hostname'], 
+                               credential['orionDB']['username'],
+                               credential['orionDB']['password'])
 
     # Initialize Jinja2 env
     env = Environment(
@@ -41,22 +47,25 @@ def main():
     #ip_sla_config_template = (open('templates/cisco_configure_icmp_ipsla.j2', 'r').read())
     j2_template = env.get_template('cisco_configure_icmp_ipsla.j2')
 
+    # Generate the IP SLA operation ID
     generate_OID(wan_node_query_list, 3)
 
+    # Destroy previous config file
     try:
         os.remove('outputs/ipsla_config.txt')
     except:
         None
 
     # Generate configuration
-    for row in wan_node_query_list:
+    for row in wan_node_query_list: # Iterate through queries
+        # Create data dict to feed to Jinja
         data = {
             'operation_number': row['OID'],
             'destination_address': row['Target Address'],
             'tag_text': row['tag']
         }
         with open('outputs/ipsla_config.txt', 'a') as f:
-            f.write(j2_template.render(data))
+            f.write(j2_template.render(data)) # Feed data to Jinja template and print to txt file
 
 if __name__ == '__main__':
     main()
